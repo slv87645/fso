eaac4b7904031779cd6eaa33511c4f48d8578e1e
Steven Vu <steven.vu0451@gmail.com>
2025-03-10 23:29:52 -0400

    Complete part 1

--- part1/exercise1/src/App.jsx
+++ part1/exercise1/src/App.jsx
@@ -6,83 +6,49 @@ const Button = (props) => {
       {props.text}
     </button>
   )
-}
-
-const Statistics = (props) => {
-  if (props.total === 0) {
-    return (
-      <div>
-        No feedback given
-      </div>
-    )
-  }
-  return (
-    <table>
-      <tbody>
-      <StatisticLine text='good' value={props.good}/>
-      <StatisticLine text='neutral' value={props.neutral}/>
-      <StatisticLine text='bad' value={props.bad}/>
-      <StatisticLine text='total' value={props.total}/>
-      <StatisticLine text='average' value={props.average}/>
-      <StatisticLine text='percentage' value={props.percentage}/>
-      </tbody>
-    </table>
-  )
 } 
 
-const StatisticLine = (props) => {
-    return (
-      <tr>
-        <td>{props.text}</td> <td>{props.value}{props.text === 'percentage' ? '%' : ''}</td>
-      </tr>
-    )
-}
-
 const App = () => {
-
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [percentage, setPercentage] = useState(0)
-
-  const handleGood = () => {
-    const updatedGood = good + 1
-    setGood(updatedGood)
-    const updatedTotal = total + 1
-    setTotal(updatedTotal)
-    setAverage(((updatedGood - bad) / updatedTotal).toFixed(1))
-    setPercentage((updatedGood / updatedTotal * 100).toFixed(1))
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  
+  const handleRandomClick = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length
+    ))
+  }
+  
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
   }
 
-  const handleNeutral = () => {
-    const updatedNeutral = neutral + 1
-    setNeutral(updatedNeutral)
-    const updatedTotal = total + 1
-    setTotal(updatedTotal)
-    setAverage(((good - bad) / updatedTotal).toFixed(1))
-    setPercentage((good / updatedTotal * 100).toFixed(1)) 
+  const indexOfMax = (arr) => {
+    return arr.reduce((maxIndex, currentValue, currentIndex, array) => {
+      return currentValue > array[maxIndex] ? currentIndex : maxIndex;
+    }, 0);
   }
-
-  const handleBad = () => {
-    const updatedBad = bad + 1
-    setBad(updatedBad)
-    const updatedTotal = total + 1
-    setTotal(updatedTotal)
-    setAverage(((good - updatedBad) / updatedTotal).toFixed(1))
-    setPercentage((good / updatedTotal * 100).toFixed(1))
-  }
-
+  
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
   return (
     <div>
-      <h1>Give feedback</h1>
-      <Button onClick={handleGood} text='good'/>
-      <Button onClick={handleNeutral} text='neutral'/>
-      <Button onClick={handleBad} text='bad'/>
-      <h1>Statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} percentage={percentage}/>
+      <h1>Anecdote of the Day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button onClick={handleVote} text='Vote'></Button>
+      <Button onClick={handleRandomClick} text="Next anecdote"/>
+      <h2>Anecdote with the most votes</h2>
+      <p>{anecdotes[indexOfMax(votes)]}</p>
     </div>
   )
 }
